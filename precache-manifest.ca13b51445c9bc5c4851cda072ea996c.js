262e54e07a8c396bb73e2751c711d29bc728b4a4
ramadika <rzdka18@gmail.com>
2020-11-05 21:50:40 +0700

    Updates

---
+++ precache-manifest.ca13b51445c9bc5c4851cda072ea996c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb61e35f5cb857e03b9235c47c3496b",
+    "url": "/mePortfolio/index.html"
+  },
+  {
+    "revision": "4ef25ed36fa80e3dd2a3",
+    "url": "/mePortfolio/static/css/main.dec40426.chunk.css"
+  },
+  {
+    "revision": "033e23932f634fd1cd54",
+    "url": "/mePortfolio/static/js/2.e8d87667.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mePortfolio/static/js/2.e8d87667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef25ed36fa80e3dd2a3",
+    "url": "/mePortfolio/static/js/main.b6722bf1.chunk.js"
+  },
+  {
+    "revision": "fe328a6385938d4e8cdb",
+    "url": "/mePortfolio/static/js/runtime-main.a30ae631.js"
+  },
+  {
+    "revision": "5355388a8923931ed73d8b330e2c7afc",
+    "url": "/mePortfolio/static/media/Aplikasi WEb.5355388a.jpg"
+  },
+  {
+    "revision": "3b0875c843103620f3d7ab30231a9c44",
+    "url": "/mePortfolio/static/media/HomepageRizkyRamadika.3b0875c8.png"
+  },
+  {
+    "revision": "e331ad9a5b09dff22e91e489b43c6bde",
+    "url": "/mePortfolio/static/media/applUIUX.e331ad9a.jpg"
+  },
+  {
+    "revision": "391f13908b0ee4ac4527a50ac29f9ca0",
+    "url": "/mePortfolio/static/media/me.391f1390.png"
+  }
+]);
